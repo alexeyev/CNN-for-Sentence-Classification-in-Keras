a572be0ed5d1d30815357af6cf0a3fba3fa8b26c
alexeyev <anton.m.alexeyev@gmail.com>
2016-09-29 11:10:54 +0300

    Args read from command line + max-over-time pooling + a few params are set as in the paper

--- main.py
+++ main.py
@@ -3,34 +3,78 @@ import numpy as np
 import data_helpers
 from model import build_compiled_model
 from w2v import train_word2vec
+import argparse as ap
+
+parser = ap.ArgumentParser(description='CNN for sentence classtion')
+
+parser.add_argument('--epochs', type=int,
+                    default=50,
+                    help='default=50; epochs count')
+
+parser.add_argument('--dataset', type=str,
+                    choices=['okstatus', 'okuser'],
+                    default='okstatus',
+                    help='default=okstatus, choose dataset')
+
+parser.add_argument('--maxlen', type=int,
+                    default=56,
+                    help='default=56; max sentence length')
+
+parser.add_argument('--optimizer', type=str, choices=['adam', 'adagrad', 'rmsprop', 'adadelta'],
+                    default='adadelta',
+                    help='default=adadelta; keras optimizer')
+
+parser.add_argument('--batch', type=int,
+                    default=50, # same as in the paper
+                    help='default=50; training batch size')
+
+parser.add_argument('--embedding_dim', type=int,
+                    default=300, # same as in the paper
+                    help='default=50; embedding size')
+
+parser.add_argument('--num_filters', type=int,
+                    default=100, # same as in the paper
+                    help='default=100; filters number')
+
+parser.add_argument('--gpu_fraction', type=float,
+                    default=0.2,
+                    help='default=0.2; GPU fraction, please, use with care')
+
+parser.add_argument('--variation', type=str, choices=['CNN-static', 'CNN-rand', 'CNN-non-static'],
+                    default='CNN-static', help='default=CNN-static')
+
+args = parser.parse_args()
+
+# ------- setting read args ---------------------
 
 np.random.seed(2)
 
-model_variation = 'CNN-static'  # CNN-rand | CNN-non-static | CNN-static
-print('Model variation is %s' % model_variation)
-
-# todo: make parametrizable from console
+model_variation = args.variation
 
 # model hyperparameters
-sequence_length = 56
-embedding_dim = 300  # 20
+# todo: should have l2 norm constraints for regularization (paper, section 2.1)
+sequence_length = int(args.maxlen)
+embedding_dim = int(args.embedding_dim)  # 20
+
 filter_sizes = (3, 4, 5)  # (3, 4)
-num_filters = 150
+num_filters = int(args.num_filters) # 150
 dropout_prob = (0.5, 0.5, 0.5)
 hidden_dims = 150
 
 # training parameters
-batch_size = 32
-num_epochs = 100
+batch_size = int(args.batch)
+num_epochs = int(args.epochs)
 val_split = 0.1
 
 # w2v parameters, see train_word2vec
+# todo: probably should not do word2vec stuff here in main.py?
 min_word_count = 1  # Minimum word count                        
 context = 10  # Context window size
 
 # Load data
 print("Loading data...")
 
+# reading texts and labels
 (xt, yt), (x_test, y_test) = data_helpers.load_ok_data_gender()
 
 xt, yt, x_test, y_test, vocabulary, vocabulary_inv = data_helpers.build_word_level_data((xt, yt), (x_test, y_test))
@@ -44,7 +88,7 @@ elif model_variation == 'CNN-rand':
 else:
     raise ValueError('Unknown model variation')
 
-# Shuffle data
+# todo: Shuffle data
 # shuffle_indices = np.random.permutation(np.arange(len(y)))
 # x_shuffled = x[shuffle_indices]
 # y_shuffled = y[shuffle_indices].argmax(axis=1)
--- model.py
+++ model.py
@@ -4,6 +4,12 @@ from keras.layers import \
     Merge, Convolution1D, MaxPooling1D
 from keras.models import Sequential, Model
 
+def max_1d(X):
+    """
+        Max-over-time pooling
+    """
+    return K.max(X, axis=1)
+
 
 def build_compiled_model(model_variation, sequence_length, embedding_dim,
                          filter_sizes, num_filters, vocabulary, embedding_weights,
@@ -20,7 +26,11 @@ def build_compiled_model(model_variation, sequence_length, embedding_dim,
                              border_mode='valid',
                              activation='relu',
                              subsample_length=1)(graph_in)
-        pool = MaxPooling1D(pool_length=2)(conv)
+        # could be like that
+        # pool = MaxPooling1D(pool_length=2)(conv)
+        # but the paper suggested max-over-time pooling
+        # see https://github.com/fchollet/keras/commit/85f80714c29d6bd8c8cde9138f0369f5df1b9a33
+        pool = Lambda(max_1d, output_shape=(num_filters,))
         flatten = Flatten()(pool)
         convs.append(flatten)
 
@@ -39,13 +49,16 @@ def build_compiled_model(model_variation, sequence_length, embedding_dim,
                             input_length=sequence_length,
                             weights=embedding_weights))
 
-    model.add(Dropout(p=dropout_prob[0], input_shape=(sequence_length, embedding_dim)))
+    # NB! the paper says there's dropout on penultimate level
+
+    # not sure whether this was in the original paper
+    # model.add(Dropout(p=dropout_prob[0], input_shape=(sequence_length, embedding_dim)))
     model.add(graph)
     model.add(Dense(output_dim=hidden_dims))
     model.add(Dropout(p=dropout_prob[1]))
     model.add(Activation(activation='relu'))
     model.add(Dense(output_dim=1))
-    # not sure
+    # not sure whether this was in the original paper
     # model.add(Dropout(p=dropout_prob[2]))
     model.add(Activation(activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
